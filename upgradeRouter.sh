117cf509f40ac4cbdc89adf6bfcc5953ab8af8e4
defead <defead@163.com>
2020-12-05 12:43:47 +0800

    up

--- upgradeRouter.sh
+++ upgradeRouter.sh
@@ -1,4 +1,9 @@
 #upgrade router firmware
+githubLatest=$(curl -so- https://raw.githubusercontent.com/defead/xiaomi4ag-RouterSnap/main/latestVersion)
 luciVersion=$(cat ./latestVersion)
-scp ./firmware/snap/${luciVersion} root@192.168.0.1:/tmp
-ssh root@192.168.0.1 sysupgrade -v /tmp/${luciVersion}
+echo $githubLatest $luciVersion
+[ $githubLatest = $luciVersion ] && exit
+wget https://github.com/defead/xiaomi4ag-RouterSnap/blob/main/firmware/snap/$githubLatest -O ./firmware/snap/$githubLatest
+echo $githubLatest > ./latestVersion
+scp ./firmware/snap/$githubLatest root@192.168.0.1:/tmp
+ssh root@192.168.0.1 sysupgrade -v /tmp/$githubLatest
