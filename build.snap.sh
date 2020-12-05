d0efe59ffecb4b34d959eb368e351d0000c51442
defead <defead@163.com>
2020-12-05 11:22:17 +0800

    add build.snap

---
+++ build.snap.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+defaultUrl=https://downloads.openwrt.org/snapshots/targets/ramips/mt7621/openwrt-ramips-mt7621-xiaomi_mi-router-4a-gigabit-squashfs-sysupgrade.bin
+builderUrl=https://downloads.openwrt.org/snapshots/targets/ramips/mt7621/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz
+profileSetting=xiaomi_mi-router-4a-gigabit
+targetBin="./bin/targets/ramips/mt7621/openwrt-ramips-mt7621-xiaomi_mi-router-4a-gigabit-squashfs-sysupgrade.bin"
+
+# cd the directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
+#check version
+rm index.html
+wget -q https://downloads.openwrt.org/snapshots/targets/ramips/mt7621/
+sLine=$(cat index.html | grep 4a-gigabit-squashfs-sysupgrade.bin)
+sTime=$(expr "$sLine" : '.*"d">\(.*\)</td>.*')
+sVersion=$(echo $sTime | awk '{printf "%s_%s", $2,$3}')
+#sVersion=Dec_2
+echo -e '--------\n'$(date)
+echo Expected version $sVersion
+[ -f ./firmware/snap/${sVersion}.bin ] && {
+    echo Version ${sVersion}.bin already exist !
+    exit
+}
+
+#download default bin
+wget $defaultUrl -O ./firmware/snap/${sVersion}.bin &
+echo [ $sTime ] .bin version >> ./versionDate
+
+#build luci version
+rm -r openwrt-imagebuilder-ramips-mt7621.Linux-x86_64*
+aria2c -s 16 -x 16 -k 1M $builderUrl
+tar -xf openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz
+cd openwrt-imagebuilder-ramips-mt7621.Linux-x86_64
+make image PROFILE=$profileSetting PACKAGES="luci"
+cp $targetBin ../firmware/snap/${sVersion}-luci.bin
+
+cd ..
+echo [ $(date) ] luci.bin build >> ./versionDate
+echo '--------------' >> ./versionDate
+
+#upgrade router firmware
+cd ..
+scp ./firmware/snap/${sVersion}-luci.bin root@192.168.0.1:/tmp
+ssh root@192.168.0.1 sysupgrade -v /tmp/${sVersion}-luci.bin
