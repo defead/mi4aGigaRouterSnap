d61fa231f00c1cb76fac41928fe1965a86e060a6
raspi <defead@163.com>
2020-12-11 20:58:07 +0800

    build failure check

--- build.snap.sh
+++ build.snap.sh
@@ -5,7 +5,7 @@ profileSetting=xiaomi_mi-router-4a-gigabit
 targetBin="./bin/targets/ramips/mt7621/openwrt-ramips-mt7621-xiaomi_mi-router-4a-gigabit-squashfs-sysupgrade.bin"
 
 # cd the directory
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 cd $DIR
 
 #check version
@@ -22,9 +22,6 @@ echo $(date '+%F %T') Expected version $sVersion
     exit
 }
 
-#download default bin
-wget $defaultUrl -nv -O ./firmware/snap/${sVersion}.bin &
-echo [ $sTime ] $sVersion.bin version >> ./versionDate
 
 #build luci version
 rm -r openwrt-imagebuilder-ramips-mt7621.Linux-x86_64*
@@ -33,15 +30,24 @@ wget -nv $builderUrl
 tar -xf openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz
 cd openwrt-imagebuilder-ramips-mt7621.Linux-x86_64
 echo building luci version ... output: buildInfo
-make image PROFILE=$profileSetting PACKAGES="luci" >> ../buildInfo.$sVersion 2>&1
+make image PROFILE=$profileSetting PACKAGES="luci" >>../buildInfo.$sVersion 2>&1
+if [ $? -eq 0 ]; then
+    echo "Successfully created $sVersion"
+else
+    echo "luciVersion build failed!!!"
+    exit
+fi
 cp $targetBin ../firmware/snap/${sVersion}-luci.bin
 
 cd ..
-echo [ $(date "+%a %b %e %R:%S %Y") ] $sVersion-luci.bin build >> ./versionDate
-echo '--------------' >> ./versionDate
+#download default bin
+wget $defaultUrl -nv -O ./firmware/snap/${sVersion}.bin &
+
+echo [ $sTime ] $sVersion.bin version >>./versionDate
+echo [ $(date "+%a %b %e %R:%S %Y") ] $sVersion-luci.bin build >>./versionDate
+echo '--------------' >>./versionDate
 
 echo ${sVersion}-luci.bin >./latestVersion
-
 
 #github push
 git pull
